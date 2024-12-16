48686f2d907e51308dfe35fef6f91b0f8b99a0f0
Michel Grothe <m.grothe@geonovum.nl>
2024-12-16 17:31:46 +0100

    Update document-config.js

--- js/document-config.js
+++ js/document-config.js
@@ -87,4 +87,73 @@ var documentConfig =
       companyURL: "https://www.geonovum.nl/"
     }
   ]
-}
+  //-- localBiblio: lokale bibliografie, voor verwijzigingen
+  //-- NB: kijk eerst naar de beschikbare www.specref.org voor verwijziging 
+  
+  localBiblio: 
+  {
+    "IDS-DSP":
+      {
+        title:      "OGC 17-069r4: OGC API - Features - Part 1: Core corrigendum, Version 1.0.1",
+        href:       "https://docs.opengeospatial.org/is/17-069r4/17-069r4.html",
+        status:     "Approved",
+        publisher:  "Open Geospatial Consortium (OGC)",
+        editors:    ["Panagiotis (Peter) A. Vretanos","Clemens Portele","Charles Heazel"],
+        date:       "2022-05-11",
+    },
+    "OAPIF1":
+      {
+        title:      "OGC 17-069r4: OGC API - Features - Part 1: Core corrigendum, Version 1.0.1",
+        href:       "https://docs.opengeospatial.org/is/17-069r4/17-069r4.html",
+        status:     "Approved",
+        publisher:  "Open Geospatial Consortium (OGC)",
+        editors:    ["Panagiotis (Peter) A. Vretanos","Clemens Portele","Charles Heazel"],
+        date:       "2022-05-11",
+    },
+    "OAPIF2":
+      {
+        title:      "OGC 18-058: OGC API - Features - Part 2: Coordinate Reference Systems by Reference, Version 1.0",
+        href:       "https://docs.opengeospatial.org/is/18-058r1/18-058r1.html",
+        status:     "Approved",
+        publisher:  "Open Geospatial Consortium (OGC)",
+        editors:    ["Panagiotis (Peter) A. Vretanos","Clemens Portele"],
+        date:       "2020-11-02",
+    },
+    "OAPIF3":
+      {
+        title:      "OGC 19-079r1: OGC API - Features - Part 3: Filtering, Version 1.0.0-rc1",
+        href:       "http://docs.ogc.org/DRAFTS/19-079r1.html",
+        status:     "Draft",
+        publisher:  "Open Geospatial Consortium (OGC)",
+        editors:    ["Panagiotis (Peter) A. Vretanos","Clemens Portele"],
+        //date:       "2022-05-11",
+    },
+    "OAPIF4":
+      {
+        title:      "OGC 20-002: OGC API - Features - Part 4: Create, Replace, Update and Delete, Version 1.0.0 snapshot (editor's draft",
+        href:       "http://docs.ogc.org/DRAFTS/20-002.html",
+        status:     "Draft",
+        publisher:  "Open Geospatial Consortium (OGC)",
+        editors:    ["Panagiotis (Peter) A. Vretanos","Clemens Portele"],
+        //date:       "2022-05-11",
+    },
+    "adr-mod-geo":
+      {
+        title:      "API Design rules Module: Geospatial",
+        href:       "https://docs.geostandaarden.nl/api/API-Strategie-mod-geo/",
+        status:     "Consultatieversie",
+        publisher:  "Geonovum",
+        editors:    ["Linda van den Brink"],
+        authors:    ["Pieter Bresters", "Linda van den Brink", "Paul van Genuchten", "George Mathijssen", "Mark Strijker"],
+        date:       "2022-09-30",
+    },
+    "CSWISOAP":
+      {
+        title:      "OGC 07-045r2: OGC Catalogue Services Specification 2.0.2 - ISO Metadata Application Profile: Corrigendum, version 1.0.2",
+        href:       "https://portal.ogc.org/files/80534",
+        status:     "Approved",
+        publisher:  "Open Geospatial Consortium (OGC)",
+        editors:    ["Uwe Voges","Kristian Senkler"],
+        date:       "2020-11-19",
+    },
+  }
