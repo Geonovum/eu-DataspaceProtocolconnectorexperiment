8ac68f518f5e434eeac026d0cda2ab28c4c2dcb2
Michel Grothe <m.grothe@geonovum.nl>
2024-12-17 12:23:17 +0100

    Update document-config.js

--- js/document-config.js
+++ js/document-config.js
@@ -77,7 +77,7 @@ var documentConfig =
     {
       name: "Gineke van Putten",
       company: "Geonovum",
-      companyURL: ""
+      companyURL: "https://www.geonovum.nl/"
     }
   ],
   editors: [
@@ -86,5 +86,332 @@ var documentConfig =
       company: "Geonovum",
       companyURL: "https://www.geonovum.nl/"
     }
-  ]
+  ],
+  localBiblio: {
+    "DONL": {
+      title: "data.overheid.nl",
+      href: "https://data.overheid.nl",
+      authors: "",
+      publisher: "Kennis- en exploitatiecentrum voor Officiële Overheidspublicaties",
+      editors: "",
+      date: "",
+      status: ""
+    },
+    "NLISO19115": 
+    {
+      title: "Nederlands metadata profiel op ISO 19115 voor geografie versie 2.1.0",
+      href: "https://docs.geostandaarden.nl/md/mdprofiel-iso19115/",
+      publisher: "Geonovum",
+      date: "30 juni 2020",
+      status: "Vastgestelde versie"
+    },
+    "NLISO19119":
+    {
+      title: "Nederlands metadata profiel op ISO 19119 voor services versie 2.1.0",
+      href: "https://docs.geostandaarden.nl/md/mdprofiel-iso19119/",
+      publisher: "Geonovum",
+      date: "30 juni 2020",
+      status: "Vastgestelde versie"
+    },
+    "ISO19115-2003":
+    {
+      title: "ISO 19115:2003 Geographic information -- Metadata",
+      href: "https://www.iso.org/standard/26020.html",
+      publisher: "International Organization for Standardization",
+      date: "May 2003",
+      status: "Adopted specification"
+    },
+    "ISO19115-2006":
+    {
+      title: "ISO 19115:2003/Cor 1:2006 Geographic information — Metadata — Technical Corrigendum 1",
+      href: "https://www.iso.org/standard/26020.html",
+      publisher: "International Organization for Standardization",
+      date: "June 2006",
+      status: "Adopted specification"
+    },
+    "NLAPIS":
+    {
+      title: "API Strategie Algemeen (Nederlandse API Strategie I)",
+      href: "https://docs.geostandaarden.nl/api/API-Strategie/",
+      publisher: "Geonovum",
+      date: "9 maart 2022",
+      status: "Vastgesteld"
+    },
+    "FE20":
+    {
+      title: "OGC 09-026r2: OGC Filter Encoding 2.0 Encoding Standard - With Corrigendum, Version 2.0.3",
+      href: "http://docs.opengeospatial.org/is/09-026r2/09-026r2.html",
+      publisher: "Open Geospatial Consortium (OGC)",
+      editors: [
+        "Panagiotis (Peter) A. Vretanos"
+      ],
+      date: "2014-08-18",
+      status: "Approved"
+    },
+    "OAPIF1":
+    {
+      title: "OGC 17-069r4: OGC API - Features - Part 1: Core corrigendum, Version 1.0.1",
+      href: "https://docs.opengeospatial.org/is/17-069r4/17-069r4.html",
+      publisher: "Open Geospatial Consortium (OGC)",
+      editors: [
+        "Panagiotis (Peter) A. Vretanos",
+        "Clemens Portele",
+        "Charles Heazel"
+      ],
+      date: "2022-05-11",
+      status: "Approved"
+    },
+    "OAPIF2":
+    {
+      title: "OGC 18-058: OGC API - Features - Part 2: Coordinate Reference Systems by Reference, Version 1.0",
+      href: "https://docs.opengeospatial.org/is/18-058r1/18-058r1.html",
+      publisher: "Open Geospatial Consortium (OGC)",
+      editors: [
+        "Panagiotis (Peter) A. Vretanos",
+        "Clemens Portele"
+      ],
+      date: "2020-11-02",
+      status: "Approved"
+    },
+    "OAPIF3":
+    {
+      title: "OGC 19-079r1: OGC API - Features - Part 3: Filtering, Version 1.0.0-rc1",
+      href: "http://docs.ogc.org/DRAFTS/19-079r1.html",
+      publisher: "Open Geospatial Consortium (OGC)",
+      editors: [
+        "Panagiotis (Peter) A. Vretanos",
+        "Clemens Portele"
+      ],
+      status: "Draft"
+    },
+    "OAPIF4":
+    {
+      title: "OGC 20-002: OGC API - Features - Part 4: Create, Replace, Update and Delete, Version 1.0.0 snapshot (editor's draft",
+      href: "http://docs.ogc.org/DRAFTS/20-002.html",
+      publisher: "Open Geospatial Consortium (OGC)",
+      editors: [
+        "Panagiotis (Peter) A. Vretanos",
+        "Clemens Portele"
+      ],
+      status: "Draft"
+    },
+    "adr-mod-geo":
+    {
+      title: "API Design rules Module: Geospatial",
+      href: "https://docs.geostandaarden.nl/api/API-Strategie-mod-geo/",
+      authors: [
+        "Pieter Bresters",
+        "Linda van den Brink",
+        "Paul van Genuchten",
+        "George Mathijssen",
+        "Mark Strijker"
+      ],
+      publisher: "Geonovum",
+      editors: [
+        "Linda van den Brink"
+      ],
+      date: "2022-09-30",
+      status: "Consultatieversie"
+    },
+    "CSWISOAP":
+    {
+      title: "OGC 07-045r2: OGC Catalogue Services Specification 2.0.2 - ISO Metadata Application Profile: Corrigendum, version 1.0.2",
+      href: "https://portal.ogc.org/files/80534",
+      publisher: "Open Geospatial Consortium (OGC)",
+      editors: [
+        "Uwe Voges",
+        "Kristian Senkler"
+      ],
+      date: "2020-11-19",
+      status: "Approved"
+    },
+    "OAPIRECORDS1":
+    {
+      title: "OGC 20-224: OGC API - Records - Part 1: Core",
+      href: "http://docs.ogc.org/DRAFTS/20-004.html",
+      publisher: "Open Geospatial Consortium (OGC)",
+      editors: [
+        "Panagiotis (Peter) A. Vretanos",
+        "Tom Kralidis",
+        "Charles Heazel"
+      ],
+      date: "2020-01-13",
+      status: "Draft"
+    },
+    "EUSD":
+    {
+      title: "A European strategy for data  ",
+      href: "https://digital-strategy.ec.europa.eu/en/policies/strategy-data",
+      publisher: "European Commission"
+    },
+    "BZK-IDS":
+    {
+      title: "NL DIGITAAL: Interbestuurlijke Datastrategie Nederland. Verantwoord gebruik van data voor maatschappelijke opgaven ",
+      href: "://www.rijksoverheid.nl/documenten/rapporten/2021/10/31/nl-digitaal-interbestuurlijke-datastrategie-nederland ",
+      publisher: "Ministerie van Economische zaken en Koninkrijksrelaties"
+    },
+    "Geonovum":
+    {
+      title: "Geonovum Rapport  - Verkenning Publiek Gebruik Data van Derden",
+      href: "https://www.geonovum.nl/uploads/documents/Rapportage%20%27Verkenning%20Publiek%20Gebruik%20Data%20van%20Derden%27%20%28eindversie%29_0.pdf ",
+      authors: [
+        "Marc de Vries en Suzanne van Poelgeest"
+      ],
+      date: "2021-06-01"
+    },
+    "TNO":
+    {
+      title: "iSHARE as generic trust framework capability",
+      href: "https://topsectorlogistiek.nl/wp-content/uploads/2022/07/TNO-2022-R11094-Report-iSHARE-as-generic-capability-1.pdf ",
+      authors: [
+        "H. Bastiaans"
+      ],
+      publisher: "TNO"
+    },
+    "IDS-CH":
+    {
+      title: "IDS Specification: Clearing House",
+      href: "https://doi.org/10.5281/zenodo.5675765",
+      authors: [
+        "Steinbuss S.",
+        "Bader S.",
+        "et al. "
+      ],
+      publisher: "International Data Spaces Association",
+      date: "2020-04-01"
+    },
+    "IDS-G":
+    {
+      title: "IDS-G on GitHub",
+      href: "https://github.com/International-Data-Spaces-Association/IDS-G",
+      publisher: "International Data Spaces Association (IDSA)"
+    },
+    "IDS-MDB":
+    {
+      title: "Specification: IDS metadata broker",
+      href: "https://internationaldataspaces.org/wp-content/uploads/dlm_uploads/IDSA-White-Paper-Specification-IDS-Meta-Data-Broker.pdf ",
+      publisher: "International Data Spaces Association (IDSA)"
+    },
+    "IDS-PPBM":
+    {
+      title: "New Business Models for Data Spaces Grounded in Data Sovereignty",
+      href: "https://internationaldataspaces.org/wp-content/uploads/IDSA-Position-Paper-New-Business-Models-sneak-preview-version.pdf ",
+      publisher: "International Data Spaces Association (IDSA)",
+      date: "2021-04-01"
+    },
+    "IDS-PPGXIDS":
+    {
+      title: "Gaia-X and IDS",
+      href: "https://internationaldataspaces.org/wp-content/uploads/dlm_uploads/IDSA-Position-Paper-GAIA-X-and-IDS.pdf",
+      publisher: "International Data Spaces Association (IDSA)"
+    },
+    "IDS-PPRB":
+    {
+      title: "IDSA Rule book",
+      href: "https://internationaldataspaces.org/wp-content/uploads/dlm_uploads/IDSA-White-Paper-IDSA-Rule-Book.pdf",
+      publisher: "International Data Spaces Association (IDSA)",
+      date: "2020-12-01"
+    },
+    "IDS-PPUC":
+    {
+      title: "Usage Control in the International Data Spaces",
+      href: "URL: https://doi.org/10.5281/zenodo.5675884",
+      authors: [
+        "Steinbuss S. et al. "
+      ],
+      publisher: "International Data Spaces Association (IDSA)"
+    },
+    "IDS-PPEUDS":
+    {
+      title: "Implementing the European Data Strategy",
+      href: "https://internationaldataspaces.org/wp-content/uploads/IDSA-Position-Paper-Implementing-European-Data-Strategy-Role-of-IDS1.pdf",
+      publisher: "International Data Spaces Association (IDSA)"
+    },
+    "IDS-PPCE":
+    {
+      title: "IDS Certification explained",
+      href: "https://doi.org/10.5281/zenodo.5675945 ",
+      authors: [
+        "Steinbuss S.",
+        "Menz N.",
+        "Resetko A.",
+        "Winkel J."
+      ]
+    },
+    "IDS-RAM3":
+    {
+      title: "IDS Reference Architecture Model (Version 3.0)",
+      href: "https://www.internationaldataspaces.org/wp-content/uploads/2019/03/IDS-Reference-Architecture-Model-3.0.pdf",
+      authors: [
+        "Otto B.",
+        "Steinbuss S.",
+        "Teuscher A.",
+        "Lohmann S. et al."
+      ],
+      publisher: "International Data Spaces Association (IDSA)"
+    },
+    "IDS-RAM4":
+    {
+      title: "IDS Reference Architecture Model (Version 4.0)",
+      href: "https://docs.internationaldataspaces.org/ids-ram-4/",
+      authors: [
+        "Otto B.",
+        "Steinbuss S.",
+        "Teuscher A.",
+        "Lohmann S. et al."
+      ],
+      publisher: "International Data Spaces Association (IDSA)"
+    },
+    "Gaia-X-TF":
+    {
+      title: "Gaia-X Trust framework",
+      href: "https://Gaia-X.eu/wp-content/uploads/2022/05/Gaia-X-Trust-Framework-22.04.pdf ",
+      publisher: "Gaia-X",
+      date: "2022-04-01"
+    },
+    "IDS-DC":
+    {
+      title: "IDS Data connectors report",
+      href: "tps://internationaldataspaces.org/wp-content/uploads/dlm_uploads/IDSA-Data-Connector-Report-5_March-2023.pdf",
+      authors: [
+        "Giulia Giussani",
+        "Sebastian Steinbuss"
+      ],
+      publisher: "International Data Spaces Association"
+    },
+    "IDS-IM":
+    {
+      title: "IDS Informationmodel Version 4.1.0.",
+      href: "https://w3id.org/idsa/core-410"
+    },
+    "DSCC-G":
+    {
+      title: "DSCC Glossary Version 1.0",
+      href: "https://dssc.eu/wp-content/uploads/2023/03/DSSC-Data-Spaces-Glossary-v1.0.pdf",
+      authors: [
+        "Data Spaces Support Centre"
+      ],
+      date: "2023-03-01"
+    },
+    "Gaia-X-G":
+    {
+      title: "Gaia-X",
+      href: "Gaia-X specifications on Gitlab"
+    },
+    "JRC126319":
+    {
+      title: "INSPIRE – A Public Sector Contribution to the European Green Deal Data Space. A vision for the technological evolution of Europe’s Spatial Data Infrastructures for 2030",
+      href: "https://publications.jrc.ec.europa.eu/repository/handle/JRC126319",
+      authors: [
+        "Kotsev A.",
+        "Minghini M.",
+        "Cetl V.",
+        "Penninga F.",
+        "Robbrecht J.",
+        "Lutz M."
+      ],
+      publisher: "Publications Office of the European Union, Luxembourg",
+      date: "2021-01-01"
+    }
+  }
 }
