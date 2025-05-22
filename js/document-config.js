698588f105da69b062853faa8cd56258f9768333
Michel Grothe <m.grothe@geonovum.nl>
2025-05-22 09:50:06 +0200

    Update document-config.js

--- js/document-config.js
+++ js/document-config.js
@@ -120,14 +120,6 @@ var documentConfig =
       date: "June 2006",
       status: "Adopted specification"
     },
-    "NLAPIS":
-    {
-      title: "API Strategie Algemeen (Nederlandse API Strategie I)",
-      href: "https://docs.geostandaarden.nl/api/API-Strategie/",
-      publisher: "Geonovum",
-      date: "9 maart 2022",
-      status: "Vastgesteld"
-    },
     "ECLPS-DSP":
     {
       title: "Dataspace Protocol 2025-1-RC1",
@@ -145,106 +137,14 @@ var documentConfig =
       date: "27 February 2025",
       status: "Draft"
     },
-    "FE20":
+    "dcat-ap-nl":
     {
-      title: "OGC 09-026r2: OGC Filter Encoding 2.0 Encoding Standard - With Corrigendum, Version 2.0.3",
-      href: "http://docs.opengeospatial.org/is/09-026r2/09-026r2.html",
-      publisher: "Open Geospatial Consortium (OGC)",
-      editors: [
-        "Panagiotis (Peter) A. Vretanos"
-      ],
-      date: "2014-08-18",
-      status: "Approved"
-    },
-    "OAPIF1":
-    {
-      title: "OGC 17-069r4: OGC API - Features - Part 1: Core corrigendum, Version 1.0.1",
-      href: "https://docs.opengeospatial.org/is/17-069r4/17-069r4.html",
-      publisher: "Open Geospatial Consortium (OGC)",
-      editors: [
-        "Panagiotis (Peter) A. Vretanos",
-        "Clemens Portele",
-        "Charles Heazel"
-      ],
-      date: "2022-05-11",
-      status: "Approved"
-    },
-    "OAPIF2":
-    {
-      title: "OGC 18-058: OGC API - Features - Part 2: Coordinate Reference Systems by Reference, Version 1.0",
-      href: "https://docs.opengeospatial.org/is/18-058r1/18-058r1.html",
-      publisher: "Open Geospatial Consortium (OGC)",
-      editors: [
-        "Panagiotis (Peter) A. Vretanos",
-        "Clemens Portele"
-      ],
-      date: "2020-11-02",
-      status: "Approved"
-    },
-    "OAPIF3":
-    {
-      title: "OGC 19-079r1: OGC API - Features - Part 3: Filtering, Version 1.0.0-rc1",
-      href: "http://docs.ogc.org/DRAFTS/19-079r1.html",
-      publisher: "Open Geospatial Consortium (OGC)",
-      editors: [
-        "Panagiotis (Peter) A. Vretanos",
-        "Clemens Portele"
-      ],
-      status: "Draft"
-    },
-    "OAPIF4":
-    {
-      title: "OGC 20-002: OGC API - Features - Part 4: Create, Replace, Update and Delete, Version 1.0.0 snapshot (editor's draft",
-      href: "http://docs.ogc.org/DRAFTS/20-002.html",
-      publisher: "Open Geospatial Consortium (OGC)",
-      editors: [
-        "Panagiotis (Peter) A. Vretanos",
-        "Clemens Portele"
-      ],
-      status: "Draft"
-    },
-    "adr-mod-geo":
-    {
-      title: "API Design rules Module: Geospatial",
-      href: "https://docs.geostandaarden.nl/api/API-Strategie-mod-geo/",
-      authors: [
-        "Pieter Bresters",
-        "Linda van den Brink",
-        "Paul van Genuchten",
-        "George Mathijssen",
-        "Mark Strijker"
-      ],
-      publisher: "Geonovum",
-      editors: [
-        "Linda van den Brink"
-      ],
-      date: "2022-09-30",
-      status: "Consultatieversie"
-    },
-    "CSWISOAP":
-    {
-      title: "OGC 07-045r2: OGC Catalogue Services Specification 2.0.2 - ISO Metadata Application Profile: Corrigendum, version 1.0.2",
-      href: "https://portal.ogc.org/files/80534",
-      publisher: "Open Geospatial Consortium (OGC)",
-      editors: [
-        "Uwe Voges",
-        "Kristian Senkler"
-      ],
-      date: "2020-11-19",
-      status: "Approved"
-    },
-    "OAPIRECORDS1":
-    {
-      title: "OGC 20-224: OGC API - Records - Part 1: Core",
-      href: "http://docs.ogc.org/DRAFTS/20-004.html",
-      publisher: "Open Geospatial Consortium (OGC)",
-      editors: [
-        "Panagiotis (Peter) A. Vretanos",
-        "Tom Kralidis",
-        "Charles Heazel"
-      ],
-      date: "2020-01-13",
-      status: "Draft"
+        title:        "DCAT-AP-NL 3.0",
+        href:         "https://docs.geostandaarden.nl/dcat/dcat-ap-nl30/",
+        status:       "Vastgestelde versie",
+        authors:      ["Ine de Visser", "Jan Skornsek"],
+        publisher:    "Geonovum",
+        date:         "12 december 2024",
     },
     "EUSD":
     {
